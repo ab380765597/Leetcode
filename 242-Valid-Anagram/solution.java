42e757bc2d794c28117a4011d3d6ccbc8fd548e1
ab380765597 <zhangxi0728@gmail.com>
2016-05-01 12:02:59 -0700

    [242. Valid Anagram][Compile Error]committed by LeetCode Extension

---
+++ 242-Valid-Anagram/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int length = s.length();
+        char sc = s.toCharArray();
+        char tc = t.toCharArray();
+        Arrays.sort(sc);
+        Arrays.sort(tc);
+        for(int i=0; i<length; i++){
+            if(sc[i]!=tc[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
