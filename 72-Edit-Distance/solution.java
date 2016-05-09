214e6fe4406a5202e14a0be3484912514f4b8f97
ab380765597 <zhangxi0728@gmail.com>
2016-05-08 17:30:11 -0700

    [72. Edit Distance][Accepted]committed by LeetCode Extension

--- 72-Edit-Distance/solution.java
+++ 72-Edit-Distance/solution.java
@@ -8,7 +8,7 @@ public class Solution {
         }
         int len1 = word1.length();
         int len2 = word2.length();
-        int[][] f = new int[len1+1][len2=1];
+        int[][] f = new int[len1+1][len2+1];
         f[0][0]  = 0;
         for(int i=1; i<=len1; i++){
             f[i][0] = i;
