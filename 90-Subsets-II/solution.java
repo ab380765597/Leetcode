3e124eecf56138a2832b389d2f006b481d101bfd
ab380765597 <zhangxi0728@gmail.com>
2016-05-14 16:37:04 -0700

    [90. Subsets II][Compile Error]committed by LeetCode Extension

--- 90-Subsets-II/solution.java
+++ 90-Subsets-II/solution.java
@@ -7,20 +7,17 @@ public class Solution {
         Arrays.sort(nums);
         List<Integer> sol = new ArrayList<Integer>();
         int length = nums.length;
-        int[] visited = new int[length];
         helper(nums, 0, res, sol, visited);
         return res;
     }
-    private void helper(int[] nums, int pos, List<List<Integer>>res, List<Integer>sol, int[] visited){
+    private void helper(int[] nums, int pos, List<List<Integer>>res, List<Integer>sol){
         res.add(new ArrayList<Integer>(sol));
         for(int i=pos; i<nums.length; i++){
             if(i!=pos && nums[i]==nums[i-1] ){
                 continue;
             }
             sol.add(nums[i]);
-            visited[i] = 1;
             helper(nums, i+1, res, sol, visited);
-            visited[i] = 0;
             sol.remove(sol.size()-1);
         }
     }
