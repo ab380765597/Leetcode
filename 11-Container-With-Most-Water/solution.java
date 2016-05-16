199c23044451cd2144c4cd23a0e4f4e98e1e6d7d
ab380765597 <zhangxi0728@gmail.com>
2016-05-16 09:38:20 -0700

    [11. Container With Most Water][Compile Error]committed by LeetCode Extension

---
+++ 11-Container-With-Most-Water/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        if(height==0 || height.length==0){
+            return 0;
+        }
+        int length = height.length;
+        int start = 0;
+        int end = length-1;
+        int maxArea = 0;
+        while(start<end){
+            maxArea = (end-start)*Math.min(height[end], height[start]);
+            if(height[start]<height[end]){
+                start++;
+            }else{
+                end--;
+            }
+        }
+        return maxArea;
+    }
+}
