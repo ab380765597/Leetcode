23918efc49bd699978b73125ef54b2ffd7ea25c8
Xi Zhang (Mike) <zhangxi0728@gmail.com>
2016-08-15 19:25:20 -0700

    [369. Plus One Linked List][Runtime Error]committed by LeetCode Extension

--- 369-Plus-One-Linked-List/solution.java
+++ 369-Plus-One-Linked-List/solution.java
@@ -8,6 +8,34 @@
  */
 public class Solution {
     public ListNode plusOne(ListNode head) {
-        
+        if(head==null){
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast!=null){
+            if(fast.val!=9){
+                fast = fast.next;
+                slow = slow.next;
+            }else{
+                fast = fast.next;
+            }
+        }
+        if(slow == head){
+            slow.val = 1;
+            slow = slow.next;
+            while(slow!=null && slow.next!=null){
+                slow.val = 0;
+                slow = slow.next;
+            }
+            slow.next = new ListNode(0);
+            return head;
+        }
+        slow.val = slow.val+1;
+        while(slow!=null){
+            slow.val = 0;
+            slow = slow.next;
+        }
+        return head;
     }
 }
