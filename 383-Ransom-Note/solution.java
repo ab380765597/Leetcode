294e767799ab395db059bbac25af731b92498dee
Xi Zhang (Mike) <zhangxi0728@gmail.com>
2016-08-21 20:22:56 -0700

    [383. Ransom Note][Compile Error]committed by LeetCode Extension

--- 383-Ransom-Note/solution.java
+++ 383-Ransom-Note/solution.java
@@ -1,5 +1,21 @@
 public class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        
+        if(ransomNote==null||ransomNote=="" && magazine==""){
+            return true;
+        }
+        int length = magazine.length();
+        int i=0;
+        int j=0;
+        for(i=0; i<length-ransomNote.length(); i++){
+            for(j=0; j<ransomNote.length(); j++){
+                if(ransomNote.charAt(j)!=magazine.charAt(i+j)){
+                    break;
+                }
+            }
+            if(j==ransomNote.length()){
+                return true;
+            }
+        }
+        return false;
     }
 }
