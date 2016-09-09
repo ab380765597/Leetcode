5225b98be765cd9ef9f9a3ae3230edb3a61ed337
Xi Zhang (Mike) <zhangxi0728@gmail.com>
2016-09-09 09:58:26 -0700

    [56. Merge Intervals][Compile Error]committed by LeetCode Extension

---
+++ 56-Merge-Intervals/solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    class IntervalComparator implements Comparator<Interval>{
+        public int compare(Interval l1, Interval l2){
+            return l1.start - l2.start;
+        }
+    }
+    
+    public List<Interval> merge(List<Interval> intervals) {
+        List<Interval> res = new ArrayList<Interval>();
+        if(intervals==null || intervals.size()==0){
+            return res;
+        }
+        Collections.sort(intervals, new IntervalComparator());
+        Interval pre = intervals.get(0);
+        for(int i=1;i<intervals.size();i++){
+            Interval cur = intervals.get(i);
+            if(pre.end>=cur.start){
+                Interval merge = new Interval(pre.start, Math.max(pre.end, cur.end));
+                pre = merge;
+            }else{
+                res.add(pre);
+                pre = cur;
+            }
+        }
+        res.add(pre);
+        return res;
+    }
+}
