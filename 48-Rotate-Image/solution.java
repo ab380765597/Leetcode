690555541cecb102b7a725bd2bf71f014e643aab
Xi Zhang (Mike) <zhangxi0728@gmail.com>
2016-09-14 21:56:46 -0700

    [48. Rotate Image][Accepted]committed by LeetCode Extension

--- 48-Rotate-Image/solution.java
+++ 48-Rotate-Image/solution.java
@@ -3,10 +3,10 @@ public class Solution {
         if(matrix==null||matrix.length==0||matrix[0].length==0){
             return;
         }
-       int n = matrix.length
+       int n = matrix.length;
         int temp = 0;
         for(int i=0; i<n; i++){
-      for(int j=0; j<n; j++){
+      for(int j=i; j<n; j++){
         temp = matrix[i][j];
         matrix[i][j] = matrix[j][i];
         matrix[j][i] = temp;
