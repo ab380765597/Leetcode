236364187637b6fbfdf205b7cbbc0ce1c71bfe41
Xi Zhang (Mike) <zhangxi0728@gmail.com>
2016-09-14 21:47:59 -0700

    [48. Rotate Image][Compile Error]committed by LeetCode Extension

--- 48-Rotate-Image/solution.java
+++ 48-Rotate-Image/solution.java
@@ -3,7 +3,7 @@ public class Solution {
         if(matrix==null||matrix.length==0||matrix[0].length==0){
             return;
         }
-       
+       int n = matrix.length
         int temp = 0;
         for(int i=0; i<n; i++){
       for(int j=0; j<n; j++){
