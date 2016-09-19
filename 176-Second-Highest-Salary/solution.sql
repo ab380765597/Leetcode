3354ba531a9d44ea71c814eff281f81ad088f312
Xi Zhang (Mike) <zhangxi0728@gmail.com>
2016-09-18 21:28:12 -0700

    [176. Second Highest Salary][Accepted]committed by LeetCode Extension

--- 176-Second-Highest-Salary/solution.sql
+++ 176-Second-Highest-Salary/solution.sql
@@ -1,7 +1,4 @@
 # Write your MySQL query statement below
-select top (1) Salary 
-from (
-    select top (2) Salary
-    from Employee
-    order by Salary DESC
-)As Emp Order by Salary;
+select MAX(Salary) as SecondHighestSalary
+from Employee
+where Salary<(select MAX(Salary)from Employee);
