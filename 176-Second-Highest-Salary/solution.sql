3c3712f1b531081485ccd890355c6ef1ccd3e9d9
Xi Zhang (Mike) <zhangxi0728@gmail.com>
2016-09-18 21:24:48 -0700

    [176. Second Highest Salary][Runtime Error]committed by LeetCode Extension

--- 176-Second-Highest-Salary/solution.sql
+++ 176-Second-Highest-Salary/solution.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-select top 1 Salary 
+select top 1 SAL 
 from (
-    select top 2 Salary 
+    select top 2 Salary as SAL
     from Employee
     order by Salary DESC
 );
